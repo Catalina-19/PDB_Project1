42642fb8fe445445b1c03f9f214d4741fef9df20
--replace-all <eric.catalina@e-campus.uab.cat>
2021-04-15 12:33:21 +0200

    parse_pdb3 corrected

--- src/parse_pdb3.py
+++ src/parse_pdb3.py
@@ -1,51 +1,48 @@
-#Import necessary packages
+# Import necessary packages
 from prody import *
 from pylab import *
 
 import os
+
 # TODO Complete this either with native prody class or manual
-standard_aa = ['ALA', 'PHE']
+# standard_aa_manual = ['ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 'ILE', 'LEU', 'LYS', 'MET',
+# 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL']
 
-#First, read all PDB files in the directory
+# First, read the text file in the directory and create an empty list (pdb_names)
 for file in os.listdir('.'):
-    if file.endswith('.pdb'):
+    if file.endswith('.txt'):
         with open(file, "rt") as f:
-            #Read the structure and get the Hierarchical View
-            structure = parsePDB(file)
-            hv = structure.getHierView()
-
-            #To iterate over every chain
-            for chain in hv:
-                #Get all residues listed into a variable, useful later
-                residues = list(chain)
-                #Set count to 0, in order to parse the residues in a window of 3
-                count = 0
-
-                for index in range(len(residues) - 3):
-                    res_i = residues[index]
-                    res_j = residues[index + 1]
-                    res_k = residues[index + 2]
-
-                    if res_i not in standard_aa and res_j not in standard_aa and res_k not in standard_aa:
-                        continue
-                    # Compute distance between atom C of res_i and atom N of res_k
-                    #   if distance < cutoff=4.0
-                    #       Compute Phi and psi
-
-                for _ in range(len(chain)-3):
-                    #Iterate over all the residues in the chain
-                    for i, residue in enumerate(residues[count:count+1]):
-                        #Take into account only amino acids, not waters or others.
-                        if residue.getResname() != "HOH" and residue.getResname() != "HEM" and residue.getResname() != "CD" and residue.getResname() != "ACT" and residues[count + 3].getResname() != "HOH":
-                            try:
-                                #Calculate the distance between the C-alphas of the residues two residues away.
-                                print("%s: Distance between C-alpha of residues %s and %s is %.2f" % (chain, residues[count], residues[count + 2], calcDistance(residues[count].getAtom("CA"), residues[count + 2].getAtom("CA"))))
-                                #Compute the Phi and the Psi of the residues in which the C-alpha distance is shorter than 3.5 Angstrom..Avoid the terminal residues(first and last, in which Phi and Psi angles cannot be calculated).
-                                #if calcDistance(residues[count].getAtom("C"), residues[count + 2].getAtom("N")) < 3.5 and residue.getResnum() != 1:
-                                if calcDistance(residues[count].getAtom("CA"), residues[count + 2].getAtom("CA")) < 3.5 and residue.getResnum() != 1:
-                                    print("%s: the Phi of the residue %s is %.2f" % (chain, residues[count], calcPhi(residues[count])))
-                                    print("%s: the Psi of the residue %s is %.2f" % (chain, residues[count], calcPsi(residues[count])))
-                            except:
-                                continue
-                    #Sum 1 to the count, to calculate over the next residue.
-                    count += 1
+            pdb_names = list()
+            # Read all the lines in the text file and append them to the pdb_names list
+            for line in f:
+                line = line.split()
+                pdb_names.append(line[0])
+            # Read each pdb file, get each structure and get the Hierarchical View of every one. Besides, define all
+            # the standard amino acids in a variable.
+            for pdb_id in pdb_names:
+                structure = parsePDB(pdb_id)
+                hv = structure.getHierView()
+                standard_aa = structure.protein.stdaa
+                # To iterate over every chain
+                for chain in hv:
+                    # Get all residues listed into a variable, useful later
+                    residues = list(chain)
+                    for index in range(len(residues) - 3):
+                        res_i = residues[index]
+                        res_j = residues[index + 1]
+                        res_k = residues[index + 2]
+                        # Check that all residues are one of the standard residues
+                        # Compute distance between atom C of res_i and atom N of res_k
+                        if res_i in standard_aa and res_j in standard_aa and res_k in standard_aa:
+                            distance_CA_N = calcDistance(residues[index].getAtom("CA"), residues[index + 2].getAtom("N"))
+                            # If distance < cutoff=4.0
+                            # Compute Phi and psi
+                            if distance_CA_N < 4.0:
+                                print("%s: Distance between the atoms C-alpha of residue %s and N of residue %s is %.2f"
+                                      % (chain, residues[index], residues[index + 2], distance_CA_N))
+                                print("%s: the Psi of the residue %s is %.2f" % (
+                                    chain, residues[index], calcPsi(residues[index])))
+                                print("%s: the Phi of the residue %s is %.2f" % (
+                                    chain, residues[index], calcPhi(residues[index])))
+                        else:
+                            continue
